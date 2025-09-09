49a07496b00258dfd17f894daec3f5935ec4fb5a
ygglue <159265464+ygglue@users.noreply.github.com>
2025-09-09 18:56:32 +0800

    finished 03 Django Views

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
