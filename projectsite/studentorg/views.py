b838c61bdc8f8ea3504efaa511406bf58e226372
ygglue <159265464+ygglue@users.noreply.github.com>
2025-09-09 20:30:15 +0800

    add organization is now working

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+class OrganizationList(ListView): 
+    model = Organization 
+    context_object_name = 'organization' 
+    template_name = 'org_list.html' 
+    paginate_by = 5
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
