c3651bd6a7fd07cda2e0d1a93e11c9e90bbe7f41
ygglue <159265464+ygglue@users.noreply.github.com>
2025-09-10 23:44:48 +0800

    fixed allowed host and requirements

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c8p+hte2+ymzs8d($r_a7sq254b&2$_4v&eh3rp93qt9p!&kr%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['ygglue.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
